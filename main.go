ec6616122db8f1bb99fdc059a42a0aaff68f9da1
Jay Boehm <jayrox@gmail.com>
2017-06-14 11:09:50 -0400

    added support for sonarr and radarr custom scripts
    support added for sonarr and radarr custom scripts. allows sonarr to set `-s true` and prbd will look for the `sonarr_episodefile_path` env variable. as well as radarr to set `-r true` and prbd will look for `radarr_moviefile_path` and prbd the file path provided. 
    added `-log <path>` to log to a central location instead of just the folder the media file is located.

--- main.go
+++ main.go
@@ -7,14 +7,18 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"time"
 )
 
 var (
-	flagdebug = flag.Bool("d", true, "show debug output")
-	flagdir   = flag.String("dir", "cwd", "directory to scan. default is current working directory (cwd)")
-	pf        prbdFlags
-	mediaext  = []string{".avi", ".mkv", ".mp4"}
+	flagDebug  = flag.Bool("d", true, "show debug output")
+	flagDir    = flag.String("dir", "cwd", "directory to scan. default is current working directory (cwd)")
+	flagLog    = flag.String("log", "cwd", "directory to write the log. default is current working directory (cwd)")
+	flagSonarr = flag.String("s", "false", "used when called from sonarr. gets the file name from the enviroment variables.")
+	flagRadarr = flag.String("r", "false", "used when called from radarr. gets the file name from the enviroment variables.")
+	pf         prbdFlags
+	mediaext   = []string{".avi", ".divx", ".m4v", ".mkv", ".mp4"}
 )
 
 func main() {
@@ -23,20 +27,63 @@ func main() {
 	printLogo()
 
 	// Root folder to scan
-	fpSAbs, _ := filepath.Abs(flagString(flagdir))
+	fpSAbs, _ := filepath.Abs(flagString(flagDir))
 	pf.Dir = fpSAbs
-	if flagString(flagdir) == "cwd" {
+	if flagString(flagDir) == "cwd" {
 		dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 		check(err)
 		pf.Dir = dir
 	}
 
-	fmt.Printf("Scanning directory: %s\n", pf.Dir)
-	fmt.Println("_____________________")
+	// Root folder to write logs
+	fpLAbs, _ := filepath.Abs(flagString(flagLog))
+	pf.Log = fpLAbs
+	if flagString(flagLog) == "cwd" {
+		dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+		check(err)
+		pf.Log = dir
+	}
 
-	i := folderWalk(pf.Dir)
-	if i < 1 {
-		fmt.Println("No media found.")
+	if flagString(flagSonarr) == "true" {
+		pf.File = os.Getenv("sonarr_episodefile_path")
+		pf.Dir = ""
+	}
+
+	if flagString(flagRadarr) == "true" {
+		pf.File = os.Getenv("radarr_moviefile_path")
+		pf.Dir = ""
+	}
+
+	if pf.File == "" {
+		fmt.Printf("Scanning directory: %s\n", pf.Dir)
+		fmt.Println("_____________________")
+
+		i := folderWalk(pf.Dir)
+		if i < 1 {
+			fmt.Println("No media found.")
+		}
+	} else {
+		if _, err := os.Stat(pf.File); os.IsNotExist(err) {
+			fmt.Println(pf.File + "- does not exist.")
+			pf.File = strings.Replace(pf.File, ".avi", ".mp4", -1)
+			pf.File = strings.Replace(pf.File, ".divx", ".mp4", -1)
+			pf.File = strings.Replace(pf.File, ".m4v", ".mp4", -1)
+			pf.File = strings.Replace(pf.File, ".mkv", ".mp4", -1)
+			fmt.Println("Trying: " + pf.File)
+		}
+
+		ok := false
+
+		ok = mediaProbe(pf.File)
+		if ok == false {
+			fmt.Println("Probe Failed - skipping media")
+		}
+
+		ok = mediaTest(pf.File)
+		if ok == false {
+			fmt.Println("Test Failed - skipping media")
+		}
+
 	}
 }
 
@@ -105,7 +152,10 @@ func mediaTest(path string) (ok bool) {
 }
 
 func probe(filename string) error {
-	cmd := exec.Command("./ffprobe", "-v", "error", "-show_format", "-show_streams", filename, "-print_format", "json")
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	check(err)
+	ffprobe := filepath.Join(dir, "ffprobe")
+	cmd := exec.Command(ffprobe, "-v", "error", "-show_format", "-show_streams", filename, "-print_format", "json")
 
 	r, err := cmd.StdoutPipe()
 	check(err)
@@ -116,12 +166,12 @@ func probe(filename string) error {
 	err = cmd.Start()
 	check(err)
 
-	fname := filename + "-probe.json"
+	fname := strings.Trim(filename, " ") + "-probe.json"
 	body, err := ioutil.ReadAll(r)
 	check(err)
 	writeToFile(fname, body)
 
-	fname2 := filename + "-probe-error.json"
+	fname2 := strings.Trim(filename, " ") + "-probe-error.json"
 	body2, err := ioutil.ReadAll(r2)
 	check(err)
 	if len(body2) > 0 {
@@ -135,7 +185,10 @@ func probe(filename string) error {
 }
 
 func analyze(filename string) error {
-	cmd := exec.Command("./ffmpeg", "-v", "error", "-i", filename, "-f", "null", "-")
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	check(err)
+	ffmpeg := filepath.Join(dir, "ffmpeg")
+	cmd := exec.Command(ffmpeg, "-v", "error", "-i", filename, "-f", "null", "-")
 
 	r, err := cmd.StdoutPipe()
 	check(err)
@@ -146,12 +199,12 @@ func analyze(filename string) error {
 	err = cmd.Start()
 	check(err)
 
-	fname := filename + "-analyze.txt"
+	fname := strings.Trim(filename, " ") + "-analyze.txt"
 	body, err := ioutil.ReadAll(r)
 	check(err)
 	writeToFile(fname, body)
 
-	fname2 := filename + "-analyze-error.txt"
+	fname2 := strings.Trim(filename, " ") + "-analyze-error.txt"
 	body2, err := ioutil.ReadAll(r2)
 	check(err)
 	if len(body2) > 0 {
@@ -165,14 +218,33 @@ func analyze(filename string) error {
 }
 
 func writeToFile(path string, data []byte) {
-	f, err := os.Create(path)
+	if filepath.Dir(path) == filepath.Dir(pf.Log) {
+		// do natta thing
+	} else {
+		path = filepath.Join(pf.Log, filepath.Base(path))
+	}
+	fmt.Println(path)
+
+	fileName := filepath.Base(path)
+	fileName = strings.Replace(fileName, "\"", "", -1)
+	fileName = strings.Replace(fileName, ":", "", -1)
+	fileName = strings.Replace(fileName, "*", "", -1)
+	fileName = strings.Replace(fileName, "?", "", -1)
+	fileName = strings.Replace(fileName, "<", "", -1)
+	fileName = strings.Replace(fileName, ">", "", -1)
+	fileName = strings.Replace(fileName, "|", "", -1)
+	fileName = strings.Trim(fileName, " ")
+
+	filePath := filepath.Dir(path)
+	path = filepath.Join(filePath, fileName)
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0660)
 	check(err)
 
 	defer f.Close()
 
-	n, err := f.Write(data)
+	_, err = f.Write(data) // _ was n
 	check(err)
-	fmt.Printf("wrote %d bytes\n", n)
+	//fmt.Printf("wrote %d bytes\n", n)
 
 	f.Sync()
 }
@@ -186,7 +258,7 @@ func check(e error) {
 
 // Only print debug output if the debug flag is true
 func printDebug(format string, vars ...interface{}) {
-	if *flagdebug {
+	if *flagDebug {
 		if vars[0] == nil {
 			fmt.Println(format)
 			return
@@ -199,6 +271,8 @@ func printDebug(format string, vars ...interface{}) {
 type prbdFlags struct {
 	Dir   string
 	Debug bool
+	File  string
+	Log   string
 }
 
 // Print the logo, obviously
